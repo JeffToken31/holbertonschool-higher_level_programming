7888cc2c6fac78c6c1fdacc01a1bec4979a3a0d7
jeff <jeffrey-31@hotmail.fr>
2025-05-06 17:09:31 +0200

    modification of conditions to print number of arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
-
-print(f"{len(argv) - 1} arguments:")
+if len(argv) == 1:
+    print(f"{len(argv) - 1} arguments.")
+elif len(argv) == 2:
+    print(f"{len(argv) - 1} argument:")
+else:
+    print(f"{len(argv) - 1} arguments:")
 for i in range(len(argv) - 1):
     print(f"{i + 1}: {argv[i + 1]}")
