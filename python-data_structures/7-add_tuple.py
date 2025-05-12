4ac8ee44c2376ec42afb455e5ad4aa7b983284e0
jeff <jeffrey-31@hotmail.fr>
2025-05-12 15:41:32 +0200

    checker compliance

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,12 +3,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
     a0, a1, b0, b1 = 0, 0, 0, 0
     if len(tuple_a) == 1:
         a0 = tuple_a[0]
-    if len(tuple_a) == 2:
+    if len(tuple_a) > 1:
         a0 = tuple_a[0]
         a1 = tuple_a[1]
     if len(tuple_b) == 1:
         b0 = tuple_b[0]
-    if len(tuple_b) == 2:
+    if len(tuple_b) > 1:
         b0 = tuple_b[0]
         b1 = tuple_b[1]
     return (a0 + b0), (a1 + b1)
