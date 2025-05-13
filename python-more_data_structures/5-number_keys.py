4cc5b1675409954bd263256c3f5f2a421f5603e6
jeff <jeffrey-31@hotmail.fr>
2025-05-13 09:40:22 +0200

    return the cout of keys in a dictionary

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    result = 0
+    for i in range(len(a_dictionary)):
+        if a_dictionary.values is not None:
+            result += 1
+    return result
