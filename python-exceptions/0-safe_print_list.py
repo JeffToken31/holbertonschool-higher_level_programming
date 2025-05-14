db6118c726b1cfb116b2e1dd819b5f06451ee753
jeff <jeffrey-31@hotmail.fr>
2025-05-14 09:06:44 +0200

    ZeroDivisionError handled

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         while i < x:
             print("{}".format(my_list[i]), end="")
             i += 1
-    except (ValueError, TypeError):
+    except IndexError:
         pass
     print("")
     return i
