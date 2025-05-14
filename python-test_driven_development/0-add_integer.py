5e4c2087d331bfb24585782160b629863d481084
jeff <jeffrey-31@hotmail.fr>
2025-05-14 16:59:49 +0200

    First try to test if i ask attemps

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""
+Module who test a function
+It include doctest
+"""
+def add_integer(a, b=98):
+    """
+    This function add two digits
+    """
+    if not isinstance(a, float) and not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, float) and not isinstance(b, int):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
