ffaceca37014d7ce9ce5618485687b58d9dc0ce6
jeff <jeffrey-31@hotmail.fr>
2025-05-14 09:49:26 +0200

    new version for checker

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
+    
     for i in range(list_length):
         try:
-            result.append(my_list_1[i] / my_list_2[i])
+            division = 0
+            division = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
         except TypeError:
@@ -11,5 +13,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-            result.append(0)
+            result.append(division)
     return result
