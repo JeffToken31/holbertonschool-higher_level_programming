2f20ee39ff8f56f6ccc5fdc2bcf5df82d17f154f
jeff <jeffrey-31@hotmail.fr>
2025-05-16 12:58:11 +0200

    add more tests

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,7 +8,6 @@ def add_integer(a, b=98):
     """
     This function add two digits
     """
-
     if not isinstance(a, float) and not isinstance(a, int):
         raise TypeError("a must be an integer")
     if not isinstance(b, float) and not isinstance(b, int):
