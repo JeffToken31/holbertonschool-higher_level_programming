78f2414b7f67aba492e0ec1c46de49bc810b625c
jeff <jeffrey-31@hotmail.fr>
2025-05-16 14:10:27 +0200

    add more test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -16,9 +16,11 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1.2, 2.3, 3.4, 4.5]), 4.5)
         self.assertEqual(max_integer([float('inf'), 2.3]), float('inf'))
 
-    def test_max_integer_with_str(self):
+    def test_max_integer_with_type(self):
         with self.assertRaises(TypeError):
             max_integer([[1, 2], 2.3, 3.4, 4.5]), 4.5
+        with self.assertRaises(TypeError):
+            max_integer([(1, 2), 2.3, 3.4, 4.5]), 4.5
 
 
 if __name__ == '__main__':
