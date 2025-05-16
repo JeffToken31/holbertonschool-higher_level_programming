f6f90c33c9fae6808b78ae346442d01c31413650
jeff <jeffrey-31@hotmail.fr>
2025-05-16 19:18:26 +0200

    clean file and add some tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,25 +6,41 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def test_max_integer_with_integer(self):
+    def test_with_integer(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([-1, 2.4, 3.2, 4]), 4)
 
-    def test_max_integer_with_negativ_integer(self):
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+    def testwith_negativ_integer(self):
+        self.assertEqual(max_integer([-1, -2, -3, -400000]), -1)
 
-    def test_max_integer_with_float(self):
-        self.assertEqual(max_integer([1.2, 2.3, 3.4, 4.5]), 4.5)
-        self.assertEqual(max_integer([float('inf'), 2.3]), float('inf'))
-        self.assertEqual(max_integer([1.2, 2.3, 3.4, 4.5]), 4.5)
+    def test_with_max_integer(self):
+        self.assertEqual(max_integer([4, 5, 10e1000000]), float('inf'))
+
+    def test_with_float(self):
+        self.assertEqual(max_integer([1.2, 2.3, 3.45151651651, 4.5]), 4.5)
+
+    def test_with_negatif_float(self):
+        self.assertEqual(max_integer([-1.2, -2.3, -3.45151651651, -4.5]), -1.2)
+
+    def test_with_mixed_allowed_type(self):
+        self.assertEqual(max_integer([-1.5654, 0x5f, -0b10101110, 0o66]), 95)
+
+    def test_with_str(self):
+        self.assertEqual(max_integer(["abc", "bac", "cadadada", "dac"]), "dac")
 
     def test_not_equal(self):
-        self.assertNotEqual(max_integer(["a", "b", "c", "d"]), "3")
-        self.assertEqual(max_integer(["a", "b", "c", "d"]), "d")
+        self.assertNotEqual(max_integer(["a", "b", "c", "d"]), 3)
 
-    def test_max_integer_with_type(self):
+    def test_with_listinlist(self):
         with self.assertRaises(TypeError):
-            max_integer([[1, 2], 2.3, 3.4, 4.5])    
+            max_integer([[1, 2, 4], 2.3, 3.4, 4.5])
+
+    def test_with_tuple(self):
+        with self.assertRaises(TypeError):
+            max_integer([(1, 2, 4), 2.3, 3.4, 4.5])
+
+    def test_with_dict(self):
+        with self.assertRaises(TypeError):
+            max_integer([{'hello': 2, 'bonjour': 6}, 2.3, 3.4, 4.5])
 
 
 if __name__ == '__main__':
