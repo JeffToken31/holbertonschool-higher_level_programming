21e321cdae1f055acbf0510fd91418add2e58730
jeff <jeffrey-31@hotmail.fr>
2025-05-26 17:20:55 +0200

    update my work in train

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+This module is just a class
+"""
+
+
+class BaseGeometry:
+    """
+    Parent class for futur class
+    """
+    def area(self):
+        """
+        Calcul area of object
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    inheritance class of BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+        Initialize a rectangle by a width and height given
+        args:
+            width(int): to define width of the rectangle
+            height(int): to define height of the rectangle
+        raises:
+            TypeError: if width and height aren't an integer
+            ValueErro: if size is less than 0
+        """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+
+    def area(self):
+        """
+        Return area of rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Format a description of object
+        """
+        new_str = "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+        return new_str
