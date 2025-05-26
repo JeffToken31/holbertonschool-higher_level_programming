bee5bf19f3ed65c2beeaec0dad047992ffb8982e
jeff <jeffrey-31@hotmail.fr>
2025-05-26 13:41:10 +0200

    raise an Execption

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module is just a class
+"""
+
+
+class BaseGeometry:
+    """
+    Parent class for futur class
+    """
+    def area(self):
+        """
+        Calcul area of object
+        """
+        raise Exception("area() is not implemented")
