f3273f7f3efe94dbeb64a5b70f4f99a26b9bf4d1
jeff <jeffrey-31@hotmail.fr>
2025-05-26 10:52:11 +0200

    Creat class and method print_sorted

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,12 +2,16 @@
 """
 This module allow to print a sorted list
 """
+
+
 class MyList(list):
     """
     Children class of list
     """
     def print_sorted(self):
         """
-        Print a sorted list of his parent
+        Print a copy of sorted list of his parent
         """
-        print
+        copy = self[:]
+        copy.sort()
+        print(copy)
