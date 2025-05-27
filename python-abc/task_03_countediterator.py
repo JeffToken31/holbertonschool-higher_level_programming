e803c13144562fa4878680dba050d8a93da4aac7
jeff <jeffrey-31@hotmail.fr>
2025-05-27 16:25:00 +0200

    add doctstring and iter

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -9,15 +9,30 @@ class CountedIterator():
     this class...
     """
     def __init__(self, item):
+        """
+        Init the counter iterator
+        """
         self.item = item
         self.iterator = iter(item)
         self.count = 1
 
     def get_count(self):
+        """
+        Allow to know the current iteration
+        """
         return self.count
 
+    def __iter__(self):
+        """
+        Return self
+        """
+        return self
+
     def __next__(self):
-        if self.count < len(self.item):
+        """
+        return the next item and icrement count
+        """
+        if self.count <= len(self.item):
             val = next(self.iterator)
             self.count += 1
             return val
