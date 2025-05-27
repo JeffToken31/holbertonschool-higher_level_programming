1e8f99f96fce56f526bc83335ba547cde22f27ff
jeff <jeffrey-31@hotmail.fr>
2025-05-27 14:00:14 +0200

    VerboseList

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module
+"""
+
+
+class VerboseList(list):
+    """
+    Children class of list
+    """
+    def append(self, item):
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, item):
+        super().extend(item)
+        print("Extended the list with [{}] items.".format(len(item)))
+
+    def remove(self, item):
+        print("Removed [{}] from the list.".format(item))
+        super().remove(item)
+
+    def pop(self, index=-1):
+        idx = super().pop(index)
+        print("Popped [{}] from the list.".format(idx))
