714c7e866d2847b93abc09f1c94a415a7a19b3db
jeff <jeffrey-31@hotmail.fr>
2025-06-04 21:16:57 +0200

    handle more more errors possible

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -25,7 +25,7 @@ class CustomObject:
         try:
             with open(filename, "wb") as f:
                 pickle.dump(self, f)
-        except (FileNotFoundError, pickle.PickleError):
+        except (FileNotFoundError, pickle.PickleError, IOError):
             return None
 
     @classmethod
@@ -36,5 +36,5 @@ class CustomObject:
         try:
             with open(filename, "rb") as f:
                 return pickle.load(f)
-        except (FileNotFoundError, pickle.UnpicklingError):
+        except (FileNotFoundError, pickle.UnpicklingError, EOFError):
             return None
