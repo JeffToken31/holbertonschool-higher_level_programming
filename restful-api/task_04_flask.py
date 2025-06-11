b15ca24ebbf55d43fb46252c410ba2481ad53af8
jeff <jeffrey-31@hotmail.fr>
2025-06-11 09:44:03 +0200

    Using flask to create API and test it with postman and REST client(ext vscode)

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+This module is an API/RESTful
+create with flask
+"""
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+users = {
+    "jane": {
+        "username": "jane",
+        "name": "Jane",
+        "age": 28,
+        "city": "Los Angeles"
+        },
+    "john": {
+        "username": "john",
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+        }
+         }
+
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+
+@app.route("/data")
+def get_users():
+    return jsonify(list(users))
+
+
+@app.route("/status")
+def get_status():
+    return "OK"
+
+
+@app.route("/users/<username>")
+def get_user(username):
+    for user in users:
+        if user == username:
+            return jsonify(users[user])
+    return jsonify({"error": "User not found"}), 404
+
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+    username = data.get("username")
+
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    if username in users:
+        return "error: User already exist", 409
+    users[username] = data
+    return jsonify({
+        "message": "User added",
+        "user": data
+    }), 201
+
+
+if __name__ == "__main__":
+    app.run()
