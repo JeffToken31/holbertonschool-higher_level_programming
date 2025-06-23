65093bdfd5080cc55ddcfea11f2d942a138976ce
jeff <jeffrey-31@hotmail.fr>
2025-06-23 15:48:53 +0200

    mistake ;

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,7 @@
-USE hbtn_0c_0
+-- Modify charset of selected column
+USE hbtn_0c_0;
+
 ALTER TABLE
     first_table
 MODIFY
-    COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
