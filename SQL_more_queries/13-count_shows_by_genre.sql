adcb6d4b730fc0a91d4ff67fdaa769baca5fb82a
jeff <jeffrey-31@hotmail.fr>
2025-06-24 14:13:19 +0200

    new try always same output

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,7 +4,7 @@ SELECT
     count(tv_show_genres.genre_id) AS number_of_shows
 FROM
     tv_genres
-    LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+    RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE
     tv_show_genres.genre_id IS NOT NULL
 GROUP BY
