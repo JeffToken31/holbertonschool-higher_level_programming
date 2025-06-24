e10a9994a95e78ac03cb900143f7ec61227d4f74
jeff <jeffrey-31@hotmail.fr>
2025-06-24 11:02:23 +0200

    research null elements

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,12 @@
--- List all show of db without a genre linked
+-- List all shows
 SELECT
     tv_shows.title,
     tv_show_genres.genre_id
 FROM
     tv_shows
-    INNER JOIN tv_show_genres ON tv_shows.id != tv_show_genres.show_id
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE
+    tv_show_genres.show_id IS NULL
+ORDER BY
+    tv_shows.title ASC,
+    tv_show_genres.genre_id ASC;
