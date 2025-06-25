0159183249ddb917b314ee7ee5ecb9b0f35ff560
jeff <jeffrey-31@hotmail.fr>
2025-06-25 10:25:58 +0200

    Better output

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -29,7 +29,7 @@ def main():
 
     cur = db.cursor()
     cur.execute("""
-                SELECT *
+                SELECT cities.id, cities.name, states.name
                 FROM cities
                 INNER JOIN states ON states.id = cities.state_id
                 ORDER BY cities.id ASC
