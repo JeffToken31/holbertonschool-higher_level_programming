ebb5b415ae7fe3ca1bf76b4dcad3023ef71de566
jeff <jeffrey-31@hotmail.fr>
2025-06-25 09:21:06 +0200

    output not coreect according checker

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -26,7 +26,7 @@ def main():
         return 1
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;")
 
     rows = cur.fetchall()
 
