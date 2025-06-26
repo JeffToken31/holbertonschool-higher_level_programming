666668c677b10355df7df0c0a3a317b5d75be729
jeff <jeffrey-31@hotmail.fr>
2025-06-26 16:51:58 +0200

    add docstring

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module 
+This module is script that allow to print desired
+element from two table
 """
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
@@ -8,7 +9,13 @@ from model_state import Base, State
 from model_city import City
 import sys
 
+
 if __name__ == "__main__":
+    """
+    This script that allow to print desired
+    element from two tables
+    """
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
