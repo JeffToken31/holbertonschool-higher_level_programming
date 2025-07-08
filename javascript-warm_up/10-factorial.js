4bc94a1a4c8ef7bb463bb2d293ac741498069134
jeff <jeffrey-31@hotmail.fr>
2025-07-08 16:50:46 +0200

    semi standar

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-function factorial(n) {
+function factorial (n) {
   if (n === 0 || n === 1) {
     return 1;
   } else {
@@ -12,5 +12,5 @@ const a = parseInt(argv[2]);
 if (isNaN(a)) {
   console.log('1');
 }
-result = factorial(a);
+const result = factorial(a);
 console.log(result);
