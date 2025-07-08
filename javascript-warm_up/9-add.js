2dc49b3c5d550ee14cb8b4e719d990816a7df489
jeff <jeffrey-31@hotmail.fr>
2025-07-08 16:22:15 +0200

    let to const

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const { parse } = require('node:path');
 const { argv } = require('node:process');
 
-let a = parseInt(argv[2]);
-let b = parseInt(argv[3]);
+const a = parseInt(argv[2]);
+const b = parseInt(argv[3]);
 
 console.log(a + b);
