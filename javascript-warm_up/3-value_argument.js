8af773998f84d5f3d07423a9e82e9314917a809f
jeff <jeffrey-31@hotmail.fr>
2025-07-08 15:01:32 +0200

    if condition modified

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (argv[3] === undefined) {
+if (argv[2] === undefined) {
   console.log('No argument');
 } else {
   for (const element of argv.slice(2)) {
