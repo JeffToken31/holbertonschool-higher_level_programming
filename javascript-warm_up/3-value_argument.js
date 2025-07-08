80070355c20145fce690efb1428c194272006498
jeff <jeffrey-31@hotmail.fr>
2025-07-08 15:00:19 +0200

    discovering undefined and slice()

---
+++ javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if (argv[3] === undefined) {
+  console.log('No argument');
+} else {
+  for (const element of argv.slice(2)) {
+    console.log(element);
+  }
+}
