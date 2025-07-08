cbb5d29d0686a1b024b053ed939bc71685897f8f
jeff <jeffrey-31@hotmail.fr>
2025-07-08 15:42:28 +0200

    simplify code

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (argv.length < 4) {
-  console.log('undefined is undefined');
-} else {
-  console.log(''.concat(argv[2], ' is ', argv[3]));
-}
+console.log(argv[2] + ' is ' + argv[3]);
