339bcb56c36053bc0a646bb954447f60dc2bfbd6
jeff <jeffrey-31@hotmail.fr>
2025-07-08 15:06:15 +0200

    checker compliance

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -4,7 +4,5 @@ const { argv } = require('node:process');
 if (argv[2] === undefined) {
   console.log('No argument');
 } else {
-  for (const element of argv.slice(2)) {
-    console.log(element);
-  }
+  console.log(argv[2]);
 }
