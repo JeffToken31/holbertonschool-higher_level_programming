236655282e28ce95f4ac088c89449ca7c9674de5
jeff <jeffrey-31@hotmail.fr>
2025-07-14 14:03:28 +0200

    drop call script

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,5 +1,7 @@
-#!/usr/bin/node
-
 const header = document.querySelector('header').addEventListener('click', function () {
-    if (header.class.Name == 'green')
+  if (header.class.Name === 'green') {
+    header.class.Name = 'red';
+  } else {
+    header.class.Name = 'green';
+  }
 });
