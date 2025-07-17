2bbd4967b41af00d768173e0f4e1d5dacdd830e2
jeff <jeffrey-31@hotmail.fr>
2025-07-17 15:31:46 +0200

    implement orm sqlalchemy on this task

---
+++ python-server_side_rendering/task_04_db.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import json, csv
+import os
+
+db_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'products.db')
+print("DB PATH:", db_path)
+print("File exists:", os.path.exists(db_path))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/jeff/holbertonschool-higher_level_programming/python-server_side_rendering/products.db'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    __tablename__ = 'Products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    category = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+filePathCSV = "products.csv"
+filePathjson = "products.json"
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    try:
+        with open('items.json', 'r', encoding='utf-8') as i:
+            datas = json.load(i)
+        return render_template('items.html', items=datas.get("items"))
+    except Exception as e:
+        print({"error": "{}".format(e)})
+
+@app.route('/products')
+def products():
+    source = request.args.get("source")
+    if not source or source not in ("json", "csv", "sql"):
+        return render_template("product_display.html", error="Wrong source")
+
+    id = request.args.get("id")
+    products = []
+    try:
+        if source == "json":
+            with open(filePathjson, 'r', encoding='utf-8') as f:
+                products = json.load(f)
+        elif source == "csv":
+            with open(filePathCSV, 'r', encoding='utf-8') as f:
+                products = list(csv.DictReader(f))
+        elif source == "sql":
+            products = [
+                {"name": p.name, "category": p.category, "price": p.price}
+                for p in Product.query.all()
+            ]
+
+        if not id:
+            return render_template('product_display.html', products=products)
+
+        for product in products:
+            if str(product.get("id")) == str(id):
+                return render_template('product_display.html', products=[product])
+
+        return render_template('product_display.html', error="Product not found")
+
+    except Exception as e:
+        print({"error": "{}".format(e)})
+        return render_template('product_display.html', error="Error loading products")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
